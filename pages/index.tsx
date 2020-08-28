23ce915e11c3fb1acc49ae094976155bd7e4f6c1
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-08-28 16:47:36 -0500

    layout built out

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,33 @@
 import Head from 'next/head';
-import Meta from 'components/meta';
+import Link from 'next/link';
+import Header from 'components/header';
+import Layout from 'components/layout';
+import Container from 'components/container';
+import { CLIENT_NAME } from 'lib/constants';
 
-const Index = () => {
-  return (
-    
-		<div>
-			<em>new portfolio who dis?</em>
-		</div>
+interface Props {
+	props: string | number;
+	preview?: boolean;
+}
+
+const About = ({ preview, props }: Props) => {
+	return (
+		<>
+			<Header props={props} />
+			<Layout preview={preview}>
+				<Head>
+					<title>{`${CLIENT_NAME} about page`}</title>
+				</Head>
+				<Container>
+					<h2 className='text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
+						<Link href='/'>
+							<a className='hover:underline text-cimaRed px-8'>About</a>
+						</Link>
+					</h2>
+				</Container>
+			</Layout>
+		</>
 	);
 };
 
-export default Index;
+export default About;
