1bf2e01829a76024fc94ecc55ca570ecd5e7c949
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-08-29 13:59:09 -0500

    updates, new header

---
+++ components/lead.tsx
@@ -0,0 +1,25 @@
+import { AndrewIcon, ArIcon } from 'components/svg-icons';
+import Link from 'next/link';
+
+const Lead = () => {
+	return (
+		<>
+			<nav
+				className='relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg mb-3'
+				style={{
+					backgroundImage: `url(https://res.cloudinary.com/asross311/image/upload/v1598726915/portfolio/Andrew_1_iudxbk.jpg)`,
+					backgroundRepeat: 'no-repeat',
+					backgroundSize: '100% 100%',
+					backgroundColor: '#EBE8E0'
+				}}
+      >
+        <div className="container px-4 mx-auto ">
+
+        </div>
+      </nav>
+		</>
+	);
+};
+
+
+export default Lead;
