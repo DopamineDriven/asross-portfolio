9868352aba527977c5347886e3133490ca9c3fca
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-08-31 06:02:58 -0500

    building out subpages

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -10,14 +10,20 @@ import markdownToHtml from 'lib/markdownToHtml';
 import Container from 'components/container';
 import PostBody from 'components/post-body';
 import PostTitle from 'components/post-title';
+import PostHeader from 'components/post-header';
 import LeadPost from 'components/lead';
 
 interface PostSlugProps {
-  post: PostType;
-  posts: PostType[];
+	post: PostType;
+	posts: PostType[];
 }
 
-const Post = () => {
+const Post = ({ post, posts }: PostSlugProps) => {
+	const morePosts = posts?.slice(1);
+	const router = useRouter();
+	if (!router.isFallback && !post?.slug) {
+		return <ErrorPage statusCode={404} />;
+	}
 	return (
 		<Fragment>
 			<LeadPost />
