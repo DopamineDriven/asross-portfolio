85c28adfde4409af39611af650b5b9941e574042
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-08-31 05:32:12 -0500

    building out subpages

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -3,3 +3,17 @@ import Date from 'components/date-formatter';
 import CoverImage from 'components/cover-image-sub-page';
 import PostTitle from 'components/post-title';
 import Author from 'types/author';
+
+interface PostHeaderProps {
+  title: string;
+	coverImage: string;
+	author: Author;
+	date: string;
+}
+
+const PostHeader = ({ author, coverImage, date, title }: PostHeaderProps) => {
+
+}
+
+
+export default PostHeader;
--- types/author.ts
+++ types/author.ts
@@ -4,6 +4,15 @@ interface Author {
 	github: string;
 	heroku: string;
 	vercel: string;
+	antdesign: string;
+	apollo: string;
+	auth0: string;
+	graphql: string;
+	next: string;
+	react: string;
+	tailwindcss: string;
+	typescript: string;
+	wordpress: string;
 }
 
 export default Author;
