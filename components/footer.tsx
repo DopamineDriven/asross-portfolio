ead2d762ee46087a2c6515b56941c4ca4387ef11
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-08-31 00:34:53 -0500

    finishing the landing page then building out the sub-pages

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,6 +2,28 @@ import Container from 'components/container';
 import { EXAMPLE_PATH } from 'lib/constants';
 import Link from 'next/link';
 
+interface FooterProps {
+	id: number;
+	href: string;
+	title: string;
+	target: string;
+}
+
+const footerConstitutents: FooterProps[] = [
+	{
+		id: 0,
+		href: 'http://duncanross.design/',
+		title: 'Design, Duncan Ross',
+		target: '__blank'
+	},
+	{
+		id: 1,
+		href: 'https://github.com/DopamineDriven/asross-portfolio',
+		title: '©2020 ALL RIGHTS RESERVED',
+		target: '__blank'
+	}
+];
+
 const Footer = () => {
 	return (
 		<footer className='bg-portfolio font-somaRoman w-full'>
@@ -16,7 +38,17 @@ const Footer = () => {
 					</div>{' '}
 					<div className='flex flex-col w-1/2 my-0 text-sm pt-1 pb-2 items-right align-top text-right float-right -mx-5'>
 						<div className='flex flex-row w-128 align-top font-somaRoman text-oneFiveBlack'>
-							<a className='flex w-full'>©2020 ALL RIGHTS RESERVED</a>
+							<a className='flex flex-row-reverse w-full uppercase'>
+								©2020 ALL RIGHTS RESERVED
+							</a>
+							<br />
+							<a
+								className='flex flex-row-reverse w-full uppercase'
+								href='http://duncanross.design/'
+								target='__blank'
+							>
+								Design, Duncan Ross
+							</a>
 						</div>
 					</div>
 				</div>
