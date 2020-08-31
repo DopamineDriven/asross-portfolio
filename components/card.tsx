c41becc48ea7ddbfc3deba21fa506ebb96c7c5dc
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-08-30 21:52:51 -0500

    ssg incoming

--- components/card.tsx
+++ components/card.tsx
@@ -1,9 +1,8 @@
 import CoverImageCard from 'components/cover-image-card';
 import Link from 'next/link';
-import Author from 'types/author';
+import ReactMarkdown from 'react-markdown/with-html';
 
 interface CardProps {
-	author: Author;
 	date: string;
 	excerpt: string;
 	slug: string;
@@ -11,14 +10,26 @@ interface CardProps {
 	title: string;
 }
 
-const Card = ({ author, date, excerpt, slug, src, title }: CardProps) => {
-  return (
-    <div className="font-somaRoman">
-      <div className="max-w-82 ">
-
-      </div>
-    </div>
-  )
+const Card = ({ date, excerpt, slug, src, title }: CardProps) => {
+	return (
+		<div className='font-somaRoman block'>
+			<div className='max-w-82 md:max-w-150 rounded-lg overflow-x-hidden overflow-y-hidden bg-portfolio shadow-lg block'>
+				<CoverImageCard slug={slug} src={src} title={title} />
+				<div className='flex flex-col text-center justify-center bg-portfolio'>
+					<div className='font-bold font-somaRoman leading-tight'>
+						<Link as={`/posts/${slug}`} href='/posts/[slug]' passHref>
+							<a
+								className='hover:underline text-center'
+								aria-label={`date-published ${date}`}
+							>
+								<ReactMarkdown escapeHtml={false} source={title} />
+							</a>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Card;
