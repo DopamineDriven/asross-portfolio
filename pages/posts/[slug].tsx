5372a11715b5bc04912e93318afa367595f27965
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-08-31 06:13:14 -0500

    building out subpages

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -7,11 +7,11 @@ import PostType from 'types/post';
 import { getPostBySlug, getAllPosts } from 'lib/api';
 import { CLIENT_NAME } from 'lib/constants';
 import markdownToHtml from 'lib/markdownToHtml';
-import Container from 'components/container';
 import PostBody from 'components/post-body';
 import PostTitle from 'components/post-title';
 import PostHeader from 'components/post-header';
 import LeadPost from 'components/lead';
+import Cards from 'components/cards';
 
 interface PostSlugProps {
 	post: PostType;
@@ -27,11 +27,75 @@ const Post = ({ post, posts }: PostSlugProps) => {
 	return (
 		<Fragment>
 			<LeadPost />
-			<Head>
-				<title>{`${CLIENT_NAME} sub-page`}</title>
-			</Head>
+			{router.isFallback ? (
+				<PostTitle>Loading…</PostTitle>
+			) : (
+				<>
+					<article className='mb-portfolioPadding'>
+						<Head>
+							<title>
+								{post.title} | Next.js Portfolio, {CLIENT_NAME}
+							</title>
+							<meta property='og:image' content={post.ogImage.url} />
+						</Head>
+						<PostHeader
+							title={post.title}
+							src={post.coverImage}
+							date={post.date}
+							author={post.author}
+						/>
+						<PostBody content={post.content} />
+					</article>
+					<div className=' max-w-cardGrid grid mx-auto content-center justify-center items-center text-center'>
+						{morePosts?.length > 0 && <Cards posts={morePosts} />}
+					</div>
+				</>
+			)}
 		</Fragment>
 	);
 };
 
 export default Post;
+
+interface Params {
+	params: {
+		slug: string;
+	};
+}
+
+export const getStaticProps = async ({ params }: Params & GetStaticProps) => {
+	const post = getPostBySlug(params.slug, [
+		'title',
+		'date',
+		'slug',
+		'author',
+		'content',
+		'ogImage',
+		'coverImage'
+	]);
+	const content = await markdownToHtml(post.content || '');
+
+	return {
+		props: {
+			post: {
+				...post,
+				content
+			}
+		}
+	};
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+	const posts = getAllPosts(['slug']);
+
+	return {
+		paths: posts.map(posts => {
+			return {
+				params: {
+					slug: posts.slug
+				}
+			};
+		}),
+		fallback: false
+	};
+};
