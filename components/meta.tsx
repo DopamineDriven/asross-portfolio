dad7ef11524057893f6daf25ad28e779b69834ee
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-02 02:12:05 -0500

    addressing accessibility flag from lighthouse;

--- components/meta.tsx
+++ components/meta.tsx
@@ -31,7 +31,7 @@ const MetaData = () => {
 				<meta name='robots' content='all' />
 				<meta
 					name='viewport'
-					content='width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0, user-scalable=no'
+					content='width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no'
 				/>
 				<meta name='format-detection' content='telephone=no' />
 				<title>Andrew Ross — Full-Stack Engineer</title>
@@ -74,3 +74,5 @@ const MetaData = () => {
 };
 
 export default MetaData;
+
+// content='width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0, user-scalable=no'
