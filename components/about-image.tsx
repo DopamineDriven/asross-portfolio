ea9611d7d476a5e7acba59e86ce2c3f923a12ca1
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-06 23:25:36 -0500

    routing about to sub-directory

--- components/about-image.tsx
+++ components/about-image.tsx
@@ -26,13 +26,20 @@ const AboutImage = ({
 	);
 	return (
 		<div className='mx-auto max-w-aboutImage500 overflow-y-hidden overflow-x-hidden bg-portfolio block'>
-			<a aria-label={title}>
+			{slug ? (
+				<Link as={`/about/${slug}`} href='/about/[slug]' passHref>
+					<a aria-label={title}>{image}</a>
+				</Link>
+			) : (
+					image
+			)}
+			{/* <a aria-label={title}>
 				<img
 					src={src}
 					alt={title}
 					className=' w-aboutImage500 max-w-aboutImage500 h-full max-h-full z-2 '
 				/>
-			</a>
+			</a> */}
 		</div>
 	);
 };
