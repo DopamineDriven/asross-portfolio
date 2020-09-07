a9e0b3ec9d711597a2186666e10acf6fdbb8515f
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-06 23:15:49 -0500

    about type added

---
+++ _about/andrew-ross.md
@@ -0,0 +1,9 @@
+---
+title: 'About'
+excerpt: 'Excerpt for About'
+coverImage: 'https://res.cloudinary.com/asross311/image/upload/v1598947180/portfolio/IMG_3824_1_8_egnkfs.jpg'
+ogImage:
+  url: 'https://res.cloudinary.com/asross311/image/upload/v1598947180/portfolio/IMG_3824_1_8_egnkfs.jpg'
+---
+
+This is some content in a markdown file for the about section. 
--- components/about-image.tsx
+++ components/about-image.tsx
@@ -11,6 +11,13 @@ const AboutImage = ({
 	src = 'https://res.cloudinary.com/asross311/image/upload/v1598947180/portfolio/IMG_3824_1_8_egnkfs.jpg',
 	title = 'Andrew Ross'
 }: AboutImageProps) => {
+	const image = (
+		<img
+			src={src}
+			alt={title}
+			className=' w-aboutImage500 max-w-aboutImage500 h-full max-h-full z-2 '
+		/>
+	);
 	return (
 		<div className='mx-auto max-w-aboutImage500 overflow-y-hidden overflow-x-hidden bg-portfolio block'>
 			<a aria-label={title}>
