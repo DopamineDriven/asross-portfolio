dc956237778697fa984c42e8736791cbb48921e3
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-06 23:55:25 -0500

    built out about sub-directory 'about me, about ____, about being a ta, etc etc'

--- components/about-coalesced.tsx
+++ components/about-coalesced.tsx
@@ -6,21 +6,22 @@ interface AboutCoalescedProps {
 }
 
 const AboutCoalesced = ({ abouts }: AboutCoalescedProps) => {
-	<section>
-		<div className='select-none grid grid-cols-1'>
-			{abouts.map(about => (
-				<AboutContent
-					key={about.slug}
-					title={about.title}
-					excerpt={about.excerpt}
-					src={about.coverImage}
-					date={about.date}
-					slug={about.slug}
-				/>
-			))}
-		</div>
-	</section>;
+	return (
+		<section>
+			<div className='select-none grid grid-cols-1'>
+				{abouts.map(about => (
+					<AboutContent
+						key={about.slug}
+						title={about.title}
+						excerpt={about.excerpt}
+						src={about.coverImage}
+						date={about.date}
+						slug={about.slug}
+					/>
+				))}
+			</div>
+		</section>
+	);
 };
 
-
-export default AboutCoalesced
+export default AboutCoalesced;
