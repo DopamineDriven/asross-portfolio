c139f4e600fbbc8f700446e6c58bcec85faff90a
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-07 02:39:18 -0500

    building out About subpage

--- pages/abouts/[slug].tsx
+++ pages/abouts/[slug].tsx
@@ -6,9 +6,21 @@ import { Fragment } from 'react';
 import { getAboutBySlug, getAllAbouts } from 'lib/about-api';
 import { CLIENT_NAME } from 'lib/constants';
 import markdownToHtml from 'lib/markdownToHtml';
-import About from 'types/about';
-import LeadSub from 'components/lead-sub';
+import AboutType from 'types/about';
+import AboutHeader from 'components/lead-sub';
 import AboutPostHeader from 'components/about-post-header';
 import AboutPostBody from 'components/about-post-body';
 import AboutPostTitle from 'components/about-post-title';
+import AboutFooter from 'components/about-post-footer';
 
+interface AboutSlugProps {
+	about: AboutType;
+	abouts: AboutType[];
+}
+
+const About = ({ about, abouts }: AboutSlugProps) => {
+	const moreAbouts = abouts?.slice(1);
+	const router = useRouter();
+};
+
+export default About;
