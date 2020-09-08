99604e7f1670d2784f4d73522680e5d585f7bda4
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-07 19:26:31 -0500

    building out blog subdirectory

---
+++ components/blog-post-title.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+
+interface BlogPostTitleProps {
+	children?: ReactNode;
+}
+
+const BlogPostTitle = ({ children }: BlogPostTitleProps) => {
+	return (
+		<div className=' md:my-portfolioPadding'>
+			{' '}
+			<h1 className='pt-portfolioDivider font-bold tracking-wide font-head md:leading-headerAbout text-center justify-center'>
+				{children}
+			</h1>
+		</div>
+	);
+};
+
+export default BlogPostTitle;
--- types/blog.ts
+++ types/blog.ts
@@ -1,16 +1,16 @@
-import Author from 'types/author'
+import Author from 'types/author';
 
 interface Blog {
-    slug: string
-    title: string
-    date: string
-    coverImage: string
-    excerpt: string
-    ogImage: {
-      url: string
-    }
-    content: string
+	slug: string;
+	title: string;
+	date: string;
+	coverImage: string;
+	articleImage: string;
+	excerpt: string;
+	ogImage: {
+		url: string;
+	};
+	content: string;
 }
-  
 
 export default Blog;
