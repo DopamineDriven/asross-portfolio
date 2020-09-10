dc78ecf1fb012759540582cbe7a84211349129b7
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-10 03:24:54 -0500

    mapping social types globally

--- components/blog-post-source-icons.tsx
+++ components/blog-post-source-icons.tsx
@@ -1,11 +1,36 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import {
+	faGithub,
+	faDev,
+	faMedium,
+	faYoutube
+} from '@fortawesome/free-brands-svg-icons';
 import { faHerokuIcon, faVercelIcon } from 'lib/fas-custom-integration';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 interface BlogPostSourceIconMeta {
-  social: string;
+	icon: IconProp;
+	social?: string;
 }
+
+interface BlogSocials {
+	github: string;
+}
+
+const IconMapping: BlogPostSourceIconMeta[] = [
+	{
+		icon: faDev,
+		social: 'dev'
+	},
+	{
+		icon: faMedium,
+		social: 'medium'
+	},
+	{
+		icon: faYoutube,
+		social: 'youtube'
+	}
+];
 
 interface PostSourceIconProps {
 	github: string;
@@ -13,74 +38,41 @@ interface PostSourceIconProps {
 	vercel: string;
 }
 
-const BlogPostSourceIcons = ({ github, heroku, vercel }: PostSourceIconProps) => {
+const BlogPostSourceIcons = ({
+	github,
+	heroku,
+	vercel
+}: PostSourceIconProps) => {
+	const iconsMapped = IconMapping.map(constituent => {
+		return (
+			<li className='align-middle' key={constituent.social}>
+				{' '}
+				{constituent.social === 'empty' ? (
+					<div className='hidden'></div>
+				) : (
+					<a
+						aria-label={`${constituent.social}`}
+						target='__blank'
+						href={constituent.social}
+						className='bg-portfolio text-black leading-normal transition-all transform delay-300 duration-1000 ease-in-out text-lg inline-block text-center items-center align-middle justify-center w-8 h-8 mr-portfolio my-2 pt-threeHalves rounded-full focus:outline-none'
+					>
+						{constituent.social === 'empty' ? (
+							<></>
+						) : (
+							<FontAwesomeIcon
+								icon={constituent.icon}
+								className='flex align-middle text-center font-extrabold hover:text-fiveOBlack pr-half  transition-all transform delay-300 duration-1000 ease-in-out '
+								size='2x'
+							/>
+						)}
+					</a>
+				)}{' '}
+			</li>
+		);
+	});
 	return (
 		<div className='align-middle justify-right text-right inline-block'>
-			<ul className='align-middle'>
-				<li className='align-middle'>
-					{' '}
-					{github === '' ? (
-						<div className='hidden'></div>
-					) : (
-						<a
-							aria-label={`source code - ${github}`}
-							target='__blank'
-							href={github}
-							className='bg-portfolio text-black leading-normal transition-all transform delay-300 duration-1000 ease-in-out text-lg inline-block text-center items-center align-middle justify-center w-8 h-8 mr-portfolio my-2 pt-threeHalves rounded-full focus:outline-none'
-						>
-							{github === '' ? (
-								<></>
-							) : (
-								<FontAwesomeIcon
-									icon={faGithub}
-									className='flex align-middle text-center font-extrabold hover:text-fiveOBlack pr-half  transition-all transform delay-300 duration-1000 ease-in-out '
-									size='2x'
-								/>
-							)}
-						</a>
-					)}{' '}
-					{heroku === 'empty' ? (
-						<div className='hidden'></div>
-					) : (
-						<a
-							aria-label={`deployed site - ${heroku}`}
-							target='__blank'
-							href={heroku}
-							className='bg-portfolio text-black leading-normal transition-all transform delay-300 duration-1000 ease-in-out text-lg inline-block text-center items-center align-middle justify-center w-8 h-8 mx-1 my-2 pt-threeHalves rounded-full focus:outline-none'
-						>
-							{heroku === 'empty' ? (
-								<></>
-							) : (
-								<FontAwesomeIcon
-									icon={faHerokuIcon}
-									className='flex align-middle text-center font-extrabold hover:text-fiveOBlack pr-half  transition-all transform delay-300 duration-1000 ease-in-out '
-									size='2x'
-								/>
-							)}
-						</a>
-					)}{' '}
-					{vercel === '' ? (
-						<></>
-					) : (
-						<a
-							aria-label={`deployed site - ${vercel}`}
-							target='__blank'
-							href={vercel}
-							className='bg-portfolio text-black leading-normal transition-all transform delay-300 duration-1000 ease-in-out text-lg inline-block text-center items-center align-middle justify-center w-8 h-8 mx-1 my-2 pt-threeHalves rounded-full focus:outline-none'
-						>
-							{vercel === '' ? (
-								<></>
-							) : (
-								<FontAwesomeIcon
-									icon={faVercelIcon}
-									className='flex align-middle text-center font-extrabold hover:text-fiveOBlack pr-half  transition-all transform delay-300 duration-1000 ease-in-out '
-									size='2x'
-								/>
-							)}
-						</a>
-					)}{' '}
-				</li>
-			</ul>
+			<ul className='align-middle'>{iconsMapped}</ul>
 		</div>
 	);
 };
