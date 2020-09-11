780b4fa9014696520ab299882357d9c2042cd863
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-11 07:10:47 -0500

    updating sub-directories globally

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import ReactMarkdown from 'react-markdown/with-html';
 
 export default class MyDocument extends Document {
 	static async getInitialProps(ctx: any) {
@@ -6,12 +7,27 @@ export default class MyDocument extends Document {
 		return { ...initialProps };
 	}
 	render() {
+		const __html: string = `
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', '${process.env.GA_TRACKING_ID}', {
+				page_path: window.location.pathname,
+			});
+	`;
 		return (
 			<Html lang='en-US'>
 				<Head>
 					<meta charSet='utf-8' />
 					<link rel='preconnect' href='https://use.typekit.net/cub6off.css' />
 					<link rel='stylesheet' href='https://use.typekit.net/cub6off.css' />
+					{/* Global Site Tag (gtag.js) - Google Analytics */}
+					<script
+						async
+						src={`https://www.googletagmanager.com/gtag/js?id=UA-177780141-1`}
+					/>
+					<script>{<ReactMarkdown escapeHtml={false} source={__html} />}</script>
 				</Head>
 				<body className='block'>
 					<Main />
