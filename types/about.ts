085202982aa6d77aa2c46dacda35ddb21bd03d11
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-11 04:04:18 -0500

    updating sub-directories globally

--- types/about.ts
+++ types/about.ts
@@ -3,8 +3,8 @@ import AboutSocial from 'types/social/about-social';
 export default interface About extends AboutSocial {
 	slug: string;
 	title: string;
-  coverImage: string;
-  articleImage: string;
+	coverImage: string;
+	articleImage: string;
 	excerpt: string;
 	date: string;
 	ogImage: {
