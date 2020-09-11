4afbbd4eff41315d1b933b8eae7c115f0a35f454
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-11 04:03:03 -0500

    updating sub-directories globally

--- components/blog-post-source-icons.tsx
+++ components/blog-post-source-icons.tsx
@@ -1,14 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDev, faMedium, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import { FC } from 'react';
-import ReactMarkdown from 'react-markdown/with-html';
-
-enum SocialEnum {
-	dev = '',
-	medium = '',
-	youtube = ''
-}
+// import { IconProp } from '@fortawesome/fontawesome-svg-core';
+// import { FC } from 'react';
+// import ReactMarkdown from 'react-markdown/with-html';
 
 interface Social {
 	dev: string;
@@ -16,37 +10,43 @@ interface Social {
 	youtube: string;
 }
 
-interface SocialExpList {
-	social: Array<Social>;
-	id: Array<number>;
-}
-interface BlogPostSourceIconMeta {
-	icon: IconProp;
-	social?: string;
-	name: SocialEnum;
-}
+// enum SocialEnum {
+// 	dev = '',
+// 	medium = '',
+// 	youtube = ''
+// }
 
-interface BlogSocials {
-	github: string;
-}
+// interface SocialExpList {
+// 	social: Array<Social>;
+// 	id: Array<number>;
+// }
+// interface BlogPostSourceIconMeta {
+// 	icon: IconProp;
+// 	social?: string;
+// 	name: SocialEnum;
+// }
 
-const IconMapping: BlogPostSourceIconMeta[] = [
-	{
-		icon: faDev,
-		social: String(),
-		name: SocialEnum.dev
-	},
-	{
-		icon: faMedium,
-		social: String(),
-		name: SocialEnum.medium
-	},
-	{
-		icon: faYoutube,
-		social: String(),
-		name: SocialEnum.youtube
-	}
-];
+// interface BlogSocials {
+// 	github: string;
+// }
+
+// const IconMapping: BlogPostSourceIconMeta[] = [
+// 	{
+// 		icon: faDev,
+// 		social: String(),
+// 		name: SocialEnum.dev
+// 	},
+// 	{
+// 		icon: faMedium,
+// 		social: String(),
+// 		name: SocialEnum.medium
+// 	},
+// 	{
+// 		icon: faYoutube,
+// 		social: String(),
+// 		name: SocialEnum.youtube
+// 	}
+// ];
 const BlogPostSourceIcons = ({ dev, medium, youtube }: Social) => {
 	// const BlogPostSourceIcons: FC<SocialExpList> = props => {
 	// const { social } = props;
@@ -55,29 +55,29 @@ const BlogPostSourceIcons = ({ dev, medium, youtube }: Social) => {
 
 	//   />;
 	// });
-	const iconsMapped = IconMapping.map(constituent => {
-		return (
-			<li className='align-middle' key={constituent.social}>
-				{' '}
-				{constituent.social === 'empty' ? (
-					<div className='hidden'></div>
-				) : (
-					<a
-						aria-label={`${constituent.social}`}
-						target='__blank'
-						href={constituent.social}
-						className='bg-portfolio text-black leading-normal transition-all transform delay-300 duration-1000 ease-in-out text-lg inline-block text-center items-center align-middle justify-center w-8 h-8 mr-portfolio my-2 pt-threeHalves rounded-full focus:outline-none'
-					>
-						<FontAwesomeIcon
-							icon={constituent.icon}
-							className='flex align-middle text-center font-extrabold hover:text-fiveOBlack pr-half  transition-all transform delay-300 duration-1000 ease-in-out '
-							size='2x'
-						/>
-					</a>
-				)}{' '}
-			</li>
-		);
-	});
+	// const iconsMapped = IconMapping.map(constituent => {
+	// 	return (
+	// 		<li className='align-middle' key={constituent.social}>
+	// 			{' '}
+	// 			{constituent.social === 'empty' ? (
+	// 				<div className='hidden'></div>
+	// 			) : (
+	// 				<a
+	// 					aria-label={`${constituent.social}`}
+	// 					target='__blank'
+	// 					href={constituent.social}
+	// 					className='bg-portfolio text-black leading-normal transition-all transform delay-300 duration-1000 ease-in-out text-lg inline-block text-center items-center align-middle justify-center w-8 h-8 mr-portfolio my-2 pt-threeHalves rounded-full focus:outline-none'
+	// 				>
+	// 					<FontAwesomeIcon
+	// 						icon={constituent.icon}
+	// 						className='flex align-middle text-center font-extrabold hover:text-fiveOBlack pr-half  transition-all transform delay-300 duration-1000 ease-in-out '
+	// 						size='2x'
+	// 					/>
+	// 				</a>
+	// 			)}{' '}
+	// 		</li>
+	// 	);
+	// });
 	return (
 		<div className='align-middle justify-right text-right inline-block'>
 			<ul className='align-middle'>
