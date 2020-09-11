c44b96c10ce61639c187e3901711e2a7aab4f851
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-11 11:30:51 -0500

    google analytics incorporated

--- lib/google-analytics.ts
+++ lib/google-analytics.ts
@@ -4,12 +4,8 @@ export const gaInit = () => {
 	ReactGA.initialize(`${process.env.GA_TRACKING_ID}`);
 };
 
-interface PageViewProps {
-	url: string;
-}
-
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages#top_of_page
-export const pageview = ({ url }: PageViewProps) => {
+export const pageview = (url: string) => {
 	ReactGA.ga('config', process.env.GA_TRACKING_ID, {
 		page_path: url
 	});
