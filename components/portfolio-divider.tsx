b066986ff7ad711573e3646b5e6cb22f9753733b
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-11 01:42:46 -0500

    called for post.postTitle instead of post.title in pages/posts/[slug].tsx

---
+++ components/portfolio-divider.tsx
@@ -0,0 +1,7 @@
+const PortfolioDivider = () => {
+	return (
+		<hr className='border-fiveOBlack w-portfolioDividerWidth max-w-portfolioDividerWidth mx-auto mt-portfolio pb-portfolioDivider' />
+	);
+};
+
+export default PortfolioDivider;
