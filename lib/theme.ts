d2c7b2013d5fc722a63ada9ce155b6f08716418c
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-14 04:59:02 -0500

    dark mode option under construction

--- lib/theme.ts
+++ lib/theme.ts
@@ -1,5 +1,22 @@
 const getThemeInit = () => {
+	if (typeof window !== 'undefined' && window.localStorage) {
+		const storedPreferences = window.localStorage.getItem('color-theme');
+		if (typeof storedPreferences === 'string') return storedPreferences;
 
-}
+		const userMedia = window.matchMedia('(prefers-color-scheme: dark)');
+		const hasMediaQueryPreference = typeof userMedia.matches === 'boolean';
+		if (hasMediaQueryPreference) {
+			return userMedia.matches ? 'dark' : 'light';
+		}
+
+	}
+	// light is default
+	return 'light';
+};
 
 export default getThemeInit;
+
+// https://jeffjadulco.com/blog/dark-mode-react-tailwind/
+// https://github.com/jeffjadulco/dark-mode-react-tailwind/blob/master/src/css/index.css
+// https://joshwcomeau.com/gatsby/dark-mode/
+// https://github.com/donavon/use-dark-mode
