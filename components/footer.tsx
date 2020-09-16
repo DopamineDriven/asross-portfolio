8d6573f1b24f8d71f7cfe8088bf8bea845023d37
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-16 00:23:43 -0500

    house cleaning

--- components/footer.tsx
+++ components/footer.tsx
@@ -91,7 +91,7 @@ const Footer = () => {
 						</div>
 						{socialMap}
 					</div>
-					<hr className='border-fiveOBlack dark:border-eaWhite w-portfolioDividerWidth max-w-portfolioDividerWidth mx-auto mt-portfolioDivider pb-portfolioDivider' />
+					<PortfolioDivider />
 					{copyRight}
 				</div>
 			</div>
