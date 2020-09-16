8960d71cc92d9f2d92904349b493cabdb461800d
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-15 23:59:28 -0500

    house cleaning

--- components/lead-sub.tsx
+++ components/lead-sub.tsx
@@ -28,7 +28,7 @@ const LeadPost = ({ title }: SubLeadProps) => {
 				<div className='block justify-between lg:justify-start w-full min-w-full z-2 md:text-customExcerpt text-customExcerptMobile tracking-tighter'>
 					<Link href={`/#home-${title}`} passHref scroll={true}>
 						<a
-							className='block align-middle justify-between mx-auto text-center mt-portfolio ml-portfolioPadding font-somaRoman text-black hover:text-fiveOBlack transition-all duration-1000 transform ease-in-out'
+							className='block align-middle justify-between mx-auto text-center mt-portfolio ml-portfolioPadding font-somaRoman text-black hover:text-fiveOBlack dark:text-white dark:hover:text-afWhite transition-all duration-2000 transform ease-in-out'
 							aria-label='return home'
 						>
 							Return Home
