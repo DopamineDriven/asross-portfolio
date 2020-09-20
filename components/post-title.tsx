46b2a82b5031826e7515ad82407a9a121bb8f64a
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-09-20 06:54:46 -0500

    global refactoring for dark mode

--- components/post-title.tsx
+++ components/post-title.tsx
@@ -7,7 +7,7 @@ interface PostTitleProps {
 const PostTitle = ({ children }: PostTitleProps) => {
 	return (
 		<div className='md:mt-paddingPostTitleTop md:mb-gapX'>
-			<h1 className='mt-portfolioDivider  tracking-wide font-head md:leading-headerAbout text-center justify-center'>
+			<h1 className='mt-portfolioDivider tracking-wide font-head md:leading-headerAbout text-center justify-center'>
 				{children}
 			</h1>
 		</div>
