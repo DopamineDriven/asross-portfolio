0a1fed61b7c830c402265a6590c5400237a1ae04
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-10-01 19:55:20 -0500

    updates

--- components/about-excerpt.tsx
+++ components/about-excerpt.tsx
@@ -6,14 +6,13 @@ interface AboutExcerptProps {
 	slug: string;
 }
 
-const AboutExcerpt = ({ excerpt, slug }: AboutExcerptProps) => {
+const AboutExcerpt = ({ excerpt, slug }: AboutExcerptProps): JSX.Element => {
 	return (
 		<div className='transition-all transform cursor-default hover:text-tertiary duration-10000 translate-y-portfolioDivider animate-pulse'>
 			<Link as={`/about/${slug}`} href='/about/[slug]' passHref scroll={true}>
 				<a
 					className='tracking-tight font-somaRoman md:text-customTitle text-customSM'
 					aria-label={`read more about andrew ross`}
-					id={`home-${excerpt}`}
 				>
 					<ReactMarkdown
 						escapeHtml={false}
